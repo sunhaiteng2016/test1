36590a2e83cd926d00ff13fbfd505864e6fad39e
Boniu <>
2020-06-02 18:17:19 +0800

    111

--- mylibrary/src/main/java/com/boniu/mylibrary/initU.java
+++ mylibrary/src/main/java/com/boniu/mylibrary/initU.java
@@ -1,8 +1,16 @@
 package com.boniu.mylibrary;
 
+import android.content.Context;
+
+import com.bun.miitmdid.core.JLibrary;
+
 public class initU {
 
-    public  static  void  init(){
-        //JLibrary.InitEntry(context);
+    public  static  void  init(Context context){
+        try {
+            JLibrary.InitEntry(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
